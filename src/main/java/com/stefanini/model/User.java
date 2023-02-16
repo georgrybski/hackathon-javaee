313f317ea7a90057e309abb2046e8d01d0293763
georgrybski <rybskigeorg@gmail.com>
2023-02-16 14:04:19 -0300

    ~Adicionando propriedades da classe User ao modelo

--- src/main/java/com/stefanini/model/User.java
+++ src/main/java/com/stefanini/model/User.java
@@ -1,2 +1,68 @@
-package com.stefanini.model;public class User {
+package com.stefanini.model;
+
+import com.stefanini.dto.UserCreationDTO;
+import com.stefanini.utils.PasswordUtils;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "USUARIO")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @NotEmpty
+    @Size(max = 50)
+    @Column(name = "NOME", nullable = false)
+    private String name;
+
+    @NotEmpty
+    @Size(min = 5, max = 20)
+    @Column(name = "LOGIN", unique = true)
+    private String login;
+
+    @NotEmpty
+    @Size(min = 10)
+    @Email(message = "Formato inválido")
+    @Column(name = "EMAIL",nullable = false, unique = true)
+    private String email;
+
+    @NotEmpty
+    @Column(name = "DATA_DE_NASCIMENTO", nullable = false)
+    private LocalDate birthDate;
+
+    @NotEmpty
+    @Column(name = "DATA_DE_CRIACAO", nullable = false, updatable = false)
+    private LocalDateTime creationTime;
+
+    @UpdateTimestamp
+    @Column(name = "DATA_DE_ATUALIZACAO")
+    private LocalDateTime updateTime;
+    @NotEmpty
+    @Column(name = "SALT", nullable = false)
+    private String salt;
+    @NotEmpty
+    @Column(name = "SENHA", nullable = false)
+    private String password;
+
+    public User(UserCreationDTO userCreationDTO) {
+        this.name = userCreationDTO.getName();
+        this.login = userCreationDTO.getLogin();
+        this.salt = PasswordUtils.generateSalt();
+        this.password = PasswordUtils.hashPassword(userCreationDTO.getPassword(), this.salt);
+        this.email = userCreationDTO.getEmail();
+        this.birthDate = userCreationDTO.getBirthDate();
+        this.creationTime = LocalDateTime.now();
+    }
+
+    public User() {}
 }
